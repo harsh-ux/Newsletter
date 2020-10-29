006f63b05d1046d666971627727282c07473854f
harsh-agarwal <agarwal.10@iitj.ac.in>
2020-10-29 23:03:41 +0530

    Update app.py
    added an endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,7 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
-
+@app.route('/newsletter', methods=["GET"])
+def get_data():
+    data = request.get_json()
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True)
